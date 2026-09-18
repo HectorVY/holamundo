47e53ebf6fdf71cb84cd6ca70a1062f5889bb6be
HectorVY <hectorvasquezff49@gmail.com>
2026-09-17 19:30:01 -0600

    cambios para login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule,CommonModule],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
