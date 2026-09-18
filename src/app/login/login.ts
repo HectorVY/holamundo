635c464adba1c5f1bf03d3e027b084ea02b8a032
HectorVY <hectorvasquezff49@gmail.com>
2026-09-17 19:35:40 -0600

    prueba de formulario

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { readonly } from '@angular/forms/signals';
 
 @Component({
   imports: [ReactiveFormsModule,FormsModule,CommonModule],
@@ -12,3 +13,16 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './login.html',
 })
 export class Login {}
+
+  formulario:FormGroup;
+  private readonly HttpClient;
+
+  constructor(private fb:FormBuilder, HttpClient){
+    this.formulario = this.fb.group(
+      {
+        correo:['',[Validators.required, Validators.correo]],
+        
+      }
+
+    )
+  }
